43c2351b68c1f7bfd25f4c242a8e98668026c3d5
Lascosck27 <gabriellascosck0909@gmail.com>
2023-09-27 21:34:47 -0300

    Update no código

--- Treinos/Program.cs
+++ Treinos/Program.cs
@@ -1,20 +1,20 @@
-﻿class Inverter
+﻿class Inverter  //criação da classe Inverter.
 {
-    public int numero1, numero2;
-    public Inverter(int num1, int num2)
+    public int numero1, numero2;    //teremos 2 variáveis do tipo int.
+    public Inverter(int num1, int num2) //o construtor vai receber 2 parametros int e os colocar nas variáveis.
     {
         numero1 = num1;
         numero2 = num2;
     }
 
-    public static Inverter operator -(Inverter inv)
+    public static Inverter operator -(Inverter inv) //sobrecarga de operador unário de negação (-).
     {
-        inv.numero1 = -inv.numero1;
-        inv.numero2 = -inv.numero2;
+        inv.numero1 = -inv.numero1; //o numero recebido terá o sinal trocado
+        inv.numero2 = -inv.numero2; //o numero recebido terá o sinal trocado
         return inv;
     }
     
-    public void Print()
+    public void Print() //série de funções para mostrar os valores com sinal trocado...
     {
         Console.WriteLine();
         Console.WriteLine("Número 1: " + numero1);
@@ -23,18 +23,10 @@
     public void PrintNegativos()
     {
         Console.WriteLine();
-        Console.WriteLine("Seus números com sinais invertidos:");
+        Console.WriteLine("Seus números com sinais invertidos:");   //tira o sinal se os dois numeros forem 0
         Console.WriteLine("Número 1: " + Math.Abs(numero1));
         Console.WriteLine("Número 2: " + Math.Abs(numero2));
     }
-    public void PrintUm()
-    {
-        Console.WriteLine();
-        Console.WriteLine("Seus números com sinais invertidos:");
-        Console.WriteLine("Número 1: " + Math.Abs(numero1));
-        Console.WriteLine("Número 2: " + numero2);
-    }
-
     public static void Main(string[] args)
     {
         Console.WriteLine("Digite um número: ");
@@ -48,11 +40,11 @@
 
         if (usernum1 == 0 && usernum2 == 0) 
         { 
-            troca.PrintNegativos();
+            troca.Print();
         }
         else if (usernum1 == 0 || usernum2 == 0)
         {
-            troca.PrintUm();
+            troca.Print();
         }
         else
         {
